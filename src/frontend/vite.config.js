a9c5a6d5f214e1452249a15406f4d91e0f519477
goemon-github <booya.8108@gmail.com>
2024-10-10 00:08:50 +0900

    create docker-container frontend

---
+++ src/frontend/vite.config.js
@@ -0,0 +1,10 @@
+export default {
+    server: {
+        //host: '127.0.0.1',  
+        host: '0.0.0.0',  
+        port: 3000,
+        watch: {
+            usePolling: true, // ファイル変更をポーリングで検知する
+        },
+    }
+  }
