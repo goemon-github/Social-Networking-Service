ca4e604ffc87030749104d0b162c82a73e0585dc
goemon-github <booya.8108@gmail.com>
2024-10-20 22:51:43 +0900

    add DBdir, frontendFiles

---
+++ src/Helpers/Settings.php
@@ -0,0 +1,17 @@
+<?php
+namespace Helers;
+
+use Exception;
+
+class Settings{
+    private const ENV_PATH = ".env";
+
+    public static function env(string $pair): string {
+        $config = parse_ini_file(dirname(__FILE__, 2) . "/". self::ENV_PATH);
+
+        if($config === false){
+            throw new Exception();
+        }
+        return $config[$pair];
+    }
+}
